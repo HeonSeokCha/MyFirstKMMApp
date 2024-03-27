13f6128d81371529f06f9c962b3a0ead8ee5444a
hyeonseok <hs960829@gmail.com>
2024-03-27 23:38:21 +0900

    modify for run xcode.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -1,6 +1,6 @@
 import UIKit
 import SwiftUI
-import ComposeApp
+import AndroidApp
 
 struct ComposeView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UIViewController {
